bab97e0406fc95ef091aa40f871f6141277b7bfa
Pedro Wood <675793+woodp@users.noreply.github.com>
2021-08-23 10:34:24 -0300

    initial code

---
+++ member-status-service.js
@@ -0,0 +1,21 @@
+const dayjs = require('dayjs');
+
+class MemberStatusService {
+
+  getDebtors(members) {
+    const threeMonthsAgo = dayjs(new Date()).subtract(2, 'months');
+    return members.filter((member) => member.balance < 0 && member.lastPayment > threeMonthsAgo);
+  }
+
+  getInactive(members) {
+    const oneYearAgo = dayjs(new Date()).subtract(1, 'years');
+    return members.filter((member) => member.lastPayment > oneYearAgo);
+  }
+
+  getActive(members) {
+    const oneMonth = dayjs(new Date()).subtract(1, 'months');
+    return members.filter((member) => member.balance >= 0 && member.lastPayment < oneMonth);
+  }
+}
+
+module.exports = MemberStatusService;
