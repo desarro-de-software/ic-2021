56fc4964c68c64dbe6859b1dea1625b6aae1a47c
Fede <fedepatagonico2021@gmail.com>
2021-08-25 20:26:26 -0300

    initial min test

--- tests/array-stats.test.js
+++ tests/array-stats.test.js
@@ -7,3 +7,25 @@ describe("array tests", () => {
     expect(prueba.average([1, 2, 3, 5, 8, 11])).toBe(5);
   });
 });
+
+
+//PRUEBA DE FEDE ===================================================================================
+/*
+min(values) {
+    return values.reduce((min, val) => (val < min ? val : min), values[0]);
+  }
+*/ 
+
+describe('Prueba de array', ()=>{
+  
+  test('numero minimo', ()=>{
+        const minimo = prueba.min([1,2,3,4,5]); 
+        expect(minimo).toBe(1);  
+        
+    });
+    test('varios 0', ()=>{
+      const negativo = prueba.min([-1,0,1]) 
+      expect(negativo).toBe(-1);
+    });
+ 
+});
